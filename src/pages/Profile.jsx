3271d78f1a4421445ea4e6d594afc43e09018dc8
Bruno Ferreira <bruno.ferreira89@hotmail.com>
2023-06-09 09:36:07 -0300

    (PROFILE) - Requisitos 57, 58 e 59 finalizados

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -1,8 +1,40 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { useHistory } from 'react-router-dom';
+import Button from '../components/Button';
 
 function Profile() {
+  const [userEmail, setUserEmail] = useState('');
+  const history = useHistory();
+
+  const getUserFromStorage = () => {
+    const { email } = JSON.parse(localStorage.getItem('user'));
+    setUserEmail(email);
+  };
+
+  useEffect(() => {
+    getUserFromStorage();
+  }, []);
+
   return (
-    <div>Profile</div>
+    <div>
+      <span data-testid="profile-email">{userEmail}</span>
+      <div>
+        <Button
+          dataTestid="profile-done-btn"
+          textContent="Done Recipes"
+          onClick={ () => history.push('/done-recipes') }
+        />
+        <Button
+          dataTestid="profile-favorite-btn"
+          textContent="Favorite Recipes"
+          onClick={ () => history.push('/favorite-recipes') }
+        />
+        <Button
+          dataTestid="profile-logout-btn"
+          textContent="Logout"
+        />
+      </div>
+    </div>
   );
 }
 
