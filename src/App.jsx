728cb01e9587ac7e8bf93970e77e5dfbedd5fb0b
Felipe Lima Coelho <felipeapoloatena@gmail.com>
2023-06-12 11:35:44 -0300

    (All) Solucionado alguns problemas de rotas e renderização

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,13 @@
 import React from 'react';
-import { Route, Switch } from 'react-router-dom';
-import RecipeDetails from './pages/RecipeDetails';
 import HeaderProvider from './context/Providers/HeaderProvider';
-import Login from './pages/Login';
-import Recipes from './pages/Recipes';
-import Header from './components/Header';
-import RecipeInProgress from './pages/RecipeInProgress';
-import FavoriteRecipes from './pages/FavoriteRecipes';
 import SearchProvider from './context/Providers/SearchProvider';
-import Profile from './pages/Profile';
-import DoneRecipes from './pages/DoneRecipes';
+import RouterSwitch from './Router';
 
 function App() {
   return (
     <HeaderProvider>
       <SearchProvider>
-        <Header />
-        <Switch>
-          <Route exact path="/" component={ Login } />
-          <Route exact path="/meals" component={ Recipes } />
-          <Route exact path="/drinks" component={ Recipes } />
-          <Route exact path="/meals/:id" component={ RecipeDetails } />
-          <Route exact path="/drinks/:id" component={ RecipeDetails } />
-          <Route exact path="/meals/:id/in-progress" component={ RecipeInProgress } />
-          <Route exact path="/drinks/:id/in-progress" component={ RecipeInProgress } />
-          <Route path="/done-recipes" component={ DoneRecipes } />
-          <Route path="/profile" component={ Profile } />
-          <Route path="/favorite-recipes" component={ FavoriteRecipes } />
-        </Switch>
+        <RouterSwitch />
       </SearchProvider>
     </HeaderProvider>
   );
