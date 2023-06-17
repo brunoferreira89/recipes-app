5a36653ed3b9f970e38856156b9489e078a0e333
Felipe Lima Coelho <felipeapoloatena@gmail.com>
2023-06-17 18:40:31 -0300

    (RECIPES) estilizado a imagem dos cards

--- src/components/RecipeCard.jsx
+++ src/components/RecipeCard.jsx
@@ -19,47 +19,57 @@ function RecipeCard() {
       {
         pageUrl === '/meals' && mealsRecipes && mealsRecipes
           .map(({ idMeal, strMealThumb, strMeal }, index) => (
-            <div key={ idMeal } data-testid={ `${index}-recipe-card` }>
-              <Link
-                onClick={ () => handleClickImage(`/meals/${idMeal}`) }
-                to={ `/meals/${idMeal}` }
+            <Link
+              key={ idMeal }
+              data-testid={ `${index}-recipe-card` }
+              className={ styles.cardContent }
+              onClick={ () => handleClickImage(`/meals/${idMeal}`) }
+              to={ `/meals/${idMeal}` }
+            >
+              <div
+                data-testid={ `${index}-card-img` }
+                className={ styles.cardImage }
+                style={ {
+                  backgroundImage: `linear-gradient(to top,
+                      rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 0.5) 25%,
+                      transparent 55%), url(${strMealThumb})`,
+                } }
+              />
+              <h3
+                data-testid={ `${index}-card-name` }
+                className={ styles.cardTitle }
               >
-                <img
-                  data-testid={ `${index}-card-img` }
-                  className={ styles.card }
-                  src={ strMealThumb }
-                  alt={ strMeal }
-                />
-                <h3
-                  data-testid={ `${index}-card-name` }
-                >
-                  { strMeal }
-                </h3>
-              </Link>
-            </div>
+                { strMeal }
+              </h3>
+            </Link>
           ))
       }
       {
         pageUrl === '/drinks' && drinksRecipes && drinksRecipes
           .map(({ idDrink, strDrinkThumb, strDrink }, index) => (
-            <div key={ idDrink } data-testid={ `${index}-recipe-card` }>
-              <Link
-                onClick={ () => handleClickImage(`/drinks/${idMeal}`) }
-                to={ `/drinks/${idDrink}` }
-              >
-                <img
-                  data-testid={ `${index}-card-img` }
-                  className={ styles.card }
-                  src={ strDrinkThumb }
-                  alt={ strDrink }
-                />
-              </Link>
+            <Link
+              key={ idDrink }
+              data-testid={ `${index}-recipe-card` }
+              className={ styles.cardContent }
+              onClick={ () => handleClickImage(`/drinks/${idDrink}`) }
+              to={ `/drinks/${idDrink}` }
+            >
+              <div
+                data-testid={ `${index}-card-img` }
+                className={ styles.cardImage }
+                style={ {
+                  backgroundImage: `linear-gradient(to top,
+                      rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 0.5) 25%,
+                      transparent 55%), url(${strDrinkThumb})`,
+                } }
+              />
               <h3
                 data-testid={ `${index}-card-name` }
+                className={ styles.cardTitle }
               >
                 { strDrink }
               </h3>
-            </div>
+            </Link>
           ))
       }
     </div>
