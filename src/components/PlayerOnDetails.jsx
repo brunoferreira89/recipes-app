b3279bb8957b519619f263d7a2baaaf8629c4997
Felipe Lima Coelho <felipeapoloatena@gmail.com>
2023-06-19 10:48:06 -0300

    (DETAILS) não exibir o player nos drinks

--- src/components/PlayerOnDetails.jsx
+++ src/components/PlayerOnDetails.jsx
@@ -7,38 +7,40 @@ import IframeYoutube from './IframeYoutube';
 function PlayerOnDetails({ mealsOrDrinks, objectPath }) {
   const [isIframeVisible, setIsIframeVisible] = useState(false);
 
-  return (
-    <div>
-      <button
-        type="button"
-        className={ styles.playContainer }
-        onClick={ () => setIsIframeVisible((state) => !state) }
-      >
-        <div
-          className={ styles.playContent }
-        >
-          <Play className={ styles.playIcon } size={ 32 } weight="fill" />
-        </div>
-      </button>
-      <section
-        className={
-          isIframeVisible ? styles.iframeActive : styles.iframeInactive
-        }
-      >
+  if (mealsOrDrinks === 'meals') {
+    return (
+      <div>
         <button
           type="button"
+          className={ styles.playContainer }
           onClick={ () => setIsIframeVisible((state) => !state) }
-          className={ styles.closeIframe }
         >
-          X
+          <div
+            className={ styles.playContent }
+          >
+            <Play className={ styles.playIcon } size={ 32 } weight="fill" />
+          </div>
         </button>
-        <IframeYoutube
-          mealsOrDrinks={ mealsOrDrinks }
-          objectPath={ objectPath }
-        />
-      </section>
-    </div>
-  );
+        <section
+          className={
+            isIframeVisible ? styles.iframeActive : styles.iframeInactive
+          }
+        >
+          <button
+            type="button"
+            onClick={ () => setIsIframeVisible((state) => !state) }
+            className={ styles.closeIframe }
+          >
+            X
+          </button>
+          <IframeYoutube
+            mealsOrDrinks={ mealsOrDrinks }
+            objectPath={ objectPath }
+          />
+        </section>
+      </div>
+    );
+  }
 }
 
 PlayerOnDetails.propTypes = {
